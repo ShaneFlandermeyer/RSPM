b6a3da39f62232f3f93a79ce4883b82bce86569d
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2021-02-21 10:40:52 -0600

    Completely overhauled the objects

--- Antenna.m
+++ Antenna.m
@@ -1,109 +1,130 @@
-classdef Antenna < matlab.mixin.Copyable
-  %% Constants
-  properties (Constant)
-    const = struct('c',299792458,'k',1.38064852e-23,'T0_k',290)
-  end
-  %% Private Parameters
+% A class representing an antenna. For now, represents a single aperture
+% with a given width, height, and mainbeam pointing vector
+
+classdef Antenna < matlab.mixin.Copyable & matlab.mixin.CustomDisplay
+  
+  %% Properties
+  
+  % Private properties
   properties (Access = private)
-    initial_param;       % First parameter updated in updateParams()
+    initial_param; % First parameter updated in updateParams()
     updated_list = {}; % List of parameters updated due to initial_param
+    % A list of antenna parameters that are angles, used when switching
+    % between radian mode and degree mode
+    angle_params = {'azimuth','elevation'};
   end
-  %% Public Parameters
-  properties
-    center_freq;           % Carrier frequency
-    num_element_horiz;     % Number of horizontal array elements
-    num_element_vert;      % Number of vertical array elements
-    spacing_element_horiz; % Horizontal interlement spacing
-    spacing_element_vert;  % Vertical interlement spacing
-    wavelength;            % Carrier wavelength
+  
+  % Antenna Properties
+  properties (Access = public)
+    angle_mode = 'Radian';
+    width = 0; % Width of the antenna aperture
+    height = 0; % Height of the antenna aperture
+    azimuth = 0; % Azimuth angle of the antenna aperture w.r.t the x axis
+    elevation = 0; % Elevation angle of the antenna aperture
+    position = [1;0;0]; % Cartesian (XYZ) antenna beam unit vector
+  end % Public properties
+  
+  % Dependent properties that are set based on the antenna properties
+  properties (Dependent)
+    area;
+  end % Dependent Properties
+  
+  %% Setter methods
+  methods
+    
+    function set.width(obj,val)
+      validateattributes(val,{'numeric'},{'positive'});
+      obj.width = val;
+    end
+    
+    function set.height(obj,val)
+      validateattributes(val,{'numeric'},{'positive'});
+      obj.height = val;
+    end
+      
+    function set.azimuth(obj,val)
+      validateattributes(val,{'numeric'},{'finite','nonnan'});
+      obj.azimuth = val;
+    end
+    
+    function set.elevation(obj,val)
+      validateattributes(val,{'numeric'},{'finite','nonnan'});
+      obj.elevation = val;
+    end
+    
+    function set.position(obj,val)
+      validateattributes(val,{'numeric'},...
+        {'vector','3d','finite','nonnan'});
+      if isrow(val) % Make this a column vector
+        val = val.';
+      end
+      val = val ./ norm(val); % Normalize to unit vector
+      obj.position = val; % Set object property
+      obj.checkIfUpdated('position',val); % Update dependent properties
+    end
+    
+    function set.angle_mode(obj,val)
+      validateattributes(val,{'string','char'},{});
+      if strncmpi(val,'Degree',1)
+        % Convert angle measures to degree
+        obj.angle_mode = 'Degree';
+        obj.convertToDegree();
+      elseif strncmpi(val,'Radian',1)
+        % Convert angle measures to radians
+        obj.angle_mode = 'Radian';
+        obj.convertToRadian();
+      end
+    end
+      
   end
-  %% Constructor
+  %% Getter methods
   methods
-    % The default constructor creates a monostatic antenna configuration.
-    % The other constructor takes a struct as an argument and assigns
-    % values to the object for all fields common to both the struct and the
-    % object.
-    function obj = Antenna(antenna_struct)
-      warning('off','MATLAB:structOnObject')
-      % Default Constructor
-      if nargin == 0
-        obj.num_element_horiz = 1;
-        obj.num_element_vert = 1;
-        obj.spacing_element_horiz = 0;
-        obj.spacing_element_vert = 0;
-        % Struct constructor
-      else
-        struct_fields = fieldnames(antenna_struct);
-        obj_fields = fieldnames(obj);
-        % If we find fields for the constant struct or private parameters,
-        % don't pass them in
-        const_idx = ismember(struct_fields,'const');
-        list_idx = ismember(struct_fields,'updated_list');
-        ori_idx = ismember(struct_fields,'initial_param');
-        if (any(const_idx))
-          struct_fields{const_idx} = [];
-        end
-        if (any(list_idx))
-          struct_fields{list_idx} = [];
-        end
-        if (any(ori_idx))
-          struct_fields{ori_idx} = [];
-        end
-        % Loop through all fields in the struct; if a field name is common
-        % to both the struct and the object, assign the value from the
-        % struct field to the object field
-        for ii = 1:length(struct_fields)
-          if any(strcmp(struct_fields{ii},obj_fields))
-            obj.(struct_fields{ii}) = antenna_struct.(struct_fields{ii});
-          end
-        end % for
-      end % if
-    end % function
-  end % methods
-  %% Setters
-  methods
-    function set.spacing_element_horiz(obj, val)
-      obj.spacing_element_horiz = val;
-      obj.checkIfUpdated('spacing_element_horiz',val);
+    
+    % Calculate the true area of the aperture
+    function val = get.area(obj)
+      val = obj.width*obj.height;
     end
-    function set.spacing_element_vert(obj, val)
-      obj.spacing_element_vert = val;
-      obj.checkIfUpdated('spacing_element_vert',val);
-    end
-    function set.num_element_horiz(obj, val)
-      obj.num_element_horiz = val;
-      obj.checkIfUpdated('num_element_horiz',val);
-    end
-    function set.num_element_vert(obj, val)
-      obj.num_element_vert = val;
-      obj.checkIfUpdated('num_element_vert',val);
-    end
-    function set.center_freq(obj, val)
-      obj.center_freq = val;
-      obj.checkIfUpdated('center_freq',val);
-    end
-    function set.wavelength(obj, val)
-      obj.wavelength = val;
-      obj.checkIfUpdated('wavelength',val);
-    end
-  end % Setters
+    
+  end
+  
   %% Private Methods
   methods (Access = private)
+    
+    % Convert all angle measures to degree
+    function convertToDegree(obj)
+      for ii = 1:numel(obj.angle_params)
+        obj.(obj.angle_params{ii}) = (180/pi)*obj.(obj.angle_params{ii});
+      end
+    end
+    
+    % Convert all angle measures to radians
+    function convertToRadian(obj)
+      for ii = 1:numel(obj.angle_params)
+        obj.(obj.angle_params{ii}) = (pi/180)*obj.(obj.angle_params{ii});
+      end
+    end
+    
+    % A helper functions to update dependent parameters that can't be
+    % marked explicitly dependent. For example, the azimuth/elevation angle
+    % of the mainbeam should be able to update the mainbeam unit vector and
+    % vice-versa, so neither can be made explicitly dependent
     function updateParams(obj, param_name, param_val)
       % No parameter value given. Exit immediately to avoid breaking things
       if isempty(param_val)
         return
       end
       
+      
       switch (param_name)
-        case 'spacing_element_horiz'
-        case 'spacing_element_vert'
-        case 'num_element_horiz'
-        case 'num_element_vert'
-        case 'center_freq'
-          obj.wavelength = obj.const.c/obj.center_freq;
-        case 'wavelength'
-          obj.center_freq = obj.const.c/obj.wavelength;
+        
+        % Mainbeam direction updated
+        % Update list
+        % -------------------------
+        % 1. Azimuth and Elevation angles
+        case 'position'
+          [obj.azimuth,obj.elevation] = ...
+            cart2sph(obj.position(1),obj.position(2),obj.position(3));
+          
       end
       
       % Original parameters have updated all its dependent parameters. We
@@ -114,13 +135,37 @@ classdef Antenna < matlab.mixin.Copyable
       end
     end % updateParams()
     
+    % Check if the given parameter has already been updated in a recursive
+    % updateParams call. If it hasn't, add it to the list and update it
     function checkIfUpdated(obj, param_name, param_val)
       if ~any(strcmp(param_name, obj.updated_list))
         obj.updated_list{end+1} = param_name;
         obj.updateParams(param_name, param_val);
       end
     end
-  end % Private Methods
+    
+  end
+  %% Hidden Methods (DO NOT EDIT THESE)
+  methods (Hidden)
+    % Sort the object properties alphabetically
+    function value = properties( obj )
+      propList = sort( builtin("properties", obj) );
+      if nargout == 0
+        disp(propList);
+      else
+        value = propList;
+      end
+    end
+    % Sort the object field names alphabetically
+    function value = fieldnames( obj )
+      value = sort( builtin( "fieldnames", obj ) );
+    end
+  end
+  methods (Access = protected)
+    function group = getPropertyGroups( obj )
+      props = properties( obj );
+      group = matlab.mixin.util.PropertyGroup( props );
+    end
+  end
   
-  %% Public Methods
-end
+end % class
--- Radar.m
+++ Radar.m
@@ -1,64 +1,150 @@
-classdef Radar < matlab.mixin.Copyable
-  
-  %% Constants
-  properties (Constant)
+classdef Radar < matlab.mixin.Copyable & RFSystem
+  %% Properties
+  % Constants
+  properties (Constant = true)
     const = struct('c',299792458,'k',1.38064852e-23,'T0_k',290)
   end
-  %% Private Parameters
+  
+  % Private properties
   properties (Access = private)
-    initial_param;       % First parameter updated in updateParams()
+    
+    initial_param; % First parameter updated in updateParams()
     updated_list = {}; % List of parameters updated due to initial_param
+    % List of parameters that should be updated when we change the scale
+    % from linear to dB or vice versa
+    % TODO: Make separate arrays for voltage v. power quantities
+    db_list = {'loss_system','noise_fig'}; 
+    
   end
-  %% Parameter objects
-  properties
+  properties (Access = public)
     antenna = Antenna();
-    target = Target();
-    waveform = Waveform();
+    prf;               % Pulse repetition frequency
+    pri;               % Pulse repetition interval
+    
   end
-  %% System parameters
-  properties
-    noise_psd; % Noise Power Spectral Density (W/Hz)
-    pwr_noise; % Noise Power (W)
-    noise_fig; % Noise figure
-    samp_rate; % Sample Rate (Samps/sec)
+  
+  % Dependent properties: These cannot be set correctly, and are
+  % automatically calculated based on other parameters
+  properties (Dependent)
+    range_unambig;
+    velocity_unambig;
+    doppler_unambig;
   end
-%% Constructor
+  
+  %% Setter methods
   methods
-    % The default constructor creates a monostatic antenna configuration.
-    % The other constructor takes a struct as an argument and assigns
-    % values to the object for all fields common to both the struct and the
-    % object.
-    function obj = Radar(radar_struct)
-      warning('off','MATLAB:structOnObject')
-      % Default Constructor
-      if nargin == 0
-        % Struct constructor
-      else
-        struct_fields = fieldnames(radar_struct);
-        obj_fields = fieldnames(obj);
-        % If we find fields for the constant struct or private parameters,
-        % don't pass them in
-        const_idx = ismember(struct_fields,'const');
-        list_idx = ismember(struct_fields,'updated_list');
-        ori_idx = ismember(struct_fields,'initial_param');
-        if (any(const_idx))
-          struct_fields{const_idx} = [];
+    function set.prf(obj,val)
+      validateattributes(val,{'numeric'},{'finite','nonnan','nonnegative'});
+      obj.prf = val;
+      obj.checkIfUpdated('prf',val);
+    end
+    
+    function set.pri(obj,val)
+      validateattributes(val,{'numeric'},{'finite','nonnan','nonnegative'});
+      obj.pri = val;
+      obj.checkIfUpdated('pri',val);
+    end
+    
+    
+  end
+  
+  %% Getter Methods
+  methods
+    
+    function val = get.velocity_unambig(obj)
+      val = obj.wavelength*obj.prf/4;
+    end
+    
+    function val = get.range_unambig(obj)
+      val = obj.const.c*obj.pri/2;
+    end
+    
+    function val = get.doppler_unambig(obj)
+      val = obj.prf/2;
+    end
+    
+  end
+  
+  %% Public Methods
+  methods
+    
+    % For each target in the list, calculate the range measured by the
+    % radar, accounting for range ambiguities;
+    function range = getMeasuredRange(obj,targets)
+      range = zeros(numel(targets),1); % Pre-allocate
+      for ii = 1:length(range)
+        range(ii) = mod(dot(targets(ii).position,obj.antenna.position),...
+          obj.range_unambig);
+      end
+    end
+    
+    % Calculate the measured doppler shift of each target in the list,
+    % accounting for ambiguities
+    function doppler = getMeasuredDoppler(obj,targets)
+      doppler = zeros(numel(targets),1); % Pre-allocate
+      for ii = 1:length(doppler)
+        % Calculate the shift that would be measured with no ambiguities
+        true_doppler = dot(targets(ii).velocity,obj.antenna.position)*...
+          2/obj.wavelength;
+        % Shift can be measured unambiguously, send it straight to the
+        % output
+        if (abs(true_doppler) < obj.prf/2)
+          doppler(ii) = true_doppler;
+        else
+          % Measured shift is ambiguous; shift it into the measurable range
+          aliased_doppler = mod(true_doppler,obj.prf);
+          doppler(ii) = aliased_doppler-obj.prf;
         end
-        if (any(list_idx))
-          struct_fields{list_idx} = [];
-        end
-        if (any(ori_idx))
-          struct_fields{ori_idx} = [];
-        end
-        % Loop through all fields in the struct; if a field name is common
-        % to both the struct and the object, assign the value from the
-        % struct field to the object field
-        for ii = 1:length(struct_fields)
-          if any(strcmp(struct_fields{ii},obj_fields))
-            obj.(struct_fields{ii}) = radar_struct.(struct_fields{ii});
-          end
-        end % for
-      end % if
-    end % function
-  end % methods
+      end
+    end
+    
+    % Calculate the velocity measured by the radar for each target in the
+    % list. In this case, it's easier to find measured doppler and convert
+    % from there
+    function velocity = getMeasuredVelocity(obj,targets)
+      doppler = obj.getMeasuredDoppler(targets);
+      velocity = doppler*obj.wavelength/2;
+    end
+    
+    
+    
+  end
+  %% Private Methods
+  methods (Access = private)
+    
+    % A helper functions to update dependent parameters that can't be
+    % marked explicitly dependent. For example, the prf should be able to
+    % update the pri and vice versa, so the user should be able to change
+    % either of them 
+    function updateParams(obj, param_name, param_val)
+      % No parameter value given. Exit immediately to avoid breaking things
+      if isempty(param_val)
+        return
+      end
+      
+      switch (param_name)
+        case 'prf'
+          obj.pri = 1/obj.prf;
+        case 'pri'
+          obj.prf = 1/obj.pri;
+      end
+      
+      % Original parameters have updated all its dependent parameters. We
+      % can safely empty the list of parameters that have already been
+      % updated for the next assignment
+      if (strcmp(obj.initial_param, param_name))
+        obj.updated_list = {};
+      end
+    end % updateParams()
+    
+    % Check if the given parameter has already been updated in a recursive
+    % updateParams call. If it hasn't, add it to the list and update it
+    function checkIfUpdated(obj, param_name, param_val)
+      if ~any(strcmp(param_name, obj.updated_list))
+        obj.updated_list{end+1} = param_name;
+        obj.updateParams(param_name, param_val);
+      end
+    end
+    
+  end
 end
